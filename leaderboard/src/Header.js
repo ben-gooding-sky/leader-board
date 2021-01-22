fc44b7f61ba91180525c1406c563176cfeb40aa7
Jack <jack.barber@sky.uk>
2021-01-22 14:48:54 +0000

    import react bootstrap

---
+++ leaderboard/src/Header.js
@@ -0,0 +1,40 @@
+import { Navbar } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <>
+  <Navbar bg="light">
+    <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+  </Navbar>
+  <br />
+  <Navbar bg="light">
+    <Navbar.Brand>Brand text</Navbar.Brand>
+  </Navbar>
+  <br />
+  <Navbar bg="dark">
+    <Navbar.Brand href="#home">
+      <img
+        src="/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+    </Navbar.Brand>
+  </Navbar>
+  <br />
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">
+      <img
+        alt=""
+        src="/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />{' '}
+      React Bootstrap
+    </Navbar.Brand>
+  </Navbar>
+</>
+    )
+}

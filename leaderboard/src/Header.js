c2ee78f093271093da5bbff5cd969a83e607d3bd
Jack <jack.barber@sky.uk>
2021-01-22 14:55:14 +0000

    Header

--- leaderboard/src/Header.js
+++ leaderboard/src/Header.js
@@ -1,40 +1,13 @@
 import { Navbar } from 'react-bootstrap';
 
-const Header = () => {
+export const Header = () => {
     return (
         <>
-  <Navbar bg="light">
-    <Navbar.Brand href="#home">Brand link</Navbar.Brand>
-  </Navbar>
-  <br />
-  <Navbar bg="light">
-    <Navbar.Brand>Brand text</Navbar.Brand>
-  </Navbar>
-  <br />
-  <Navbar bg="dark">
-    <Navbar.Brand href="#home">
-      <img
-        src="/logo.svg"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="React Bootstrap logo"
-      />
-    </Navbar.Brand>
-  </Navbar>
-  <br />
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">
-      <img
-        alt=""
-        src="/logo.svg"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-      />{' '}
-      React Bootstrap
-    </Navbar.Brand>
-  </Navbar>
-</>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">
+                    Hades Leaderboard
+                </Navbar.Brand>
+            </Navbar>
+        </>
     )
 }
